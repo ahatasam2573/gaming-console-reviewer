4fbf1ddd8210a59f706282dbd1e470ef2da7d440
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-04-04 12:07:26 +0600

    component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Review from "./Components/Review/Review";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Blog from "./Components/Blog/Blog";
+import About from "./Components/About/About";
+import Error from "./Components/Error/Error";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Review></Review>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blogs" element={<Blog></Blog>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<Error></Error>}></Route>
+      </Routes>
     </div>
   );
 }
