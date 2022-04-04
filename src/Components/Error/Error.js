2520a2094e7598bcf0d77c55000bbfca7ebf7067
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-04-04 14:45:26 +0600

    Error page and home button added

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "./Error.css";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
+  const backToHome = () => {
+    const path = `/home`;
+    navigate(path);
+  };
   return (
-    <div>
-      <h1>
-        404 <br /> Page Not Found
-      </h1>
+    <div className="error-div">
+      <button onClick={() => backToHome()} className="error-btn">
+        Back to Home
+      </button>
+      <img src="./error.jpg" alt="" />
     </div>
   );
 };
